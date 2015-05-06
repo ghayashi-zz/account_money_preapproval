84a59b3846e843c3debad3c6eca876fa43fd90d8
Gabriel matsuoka <gabriel.matsuoka@gmail.com>
2015-05-06 20:21:30 +0000

    update db

--- web/Lib/Db.php
+++ web/Lib/Db.php
@@ -12,10 +12,10 @@ class DB{
     public function __construct() {
         
         //caso exista variaveis do servidor, usa elas
-        $this->host = isset($_REQUEST['HOST_DB']) ? $_REQUEST['HOST_DB'] : $this->host;
-        $this->user = isset($_REQUEST['USER_DB']) ? $_REQUEST['USER_DB'] : $this->user;
-        $this->pass = isset($_REQUEST['PASS_DB']) ? $_REQUEST['PASS_DB'] : $this->pass;
-        $this->db = isset($_REQUEST['NAME_DB']) ? $_REQUEST['NAME_DB'] : $this->db;
+        $this->host = isset($_SERVER['HOST_DB']) ? $_SERVER['HOST_DB'] : $this->host;
+        $this->user = isset($_SERVER['USER_DB']) ? $_SERVER['USER_DB'] : $this->user;
+        $this->pass = isset($_SERVER['PASS_DB']) ? $_SERVER['PASS_DB'] : $this->pass;
+        $this->db = isset($_SERVER['NAME_DB']) ? $_SERVER['NAME_DB'] : $this->db;
         
         print_r($this);exit;
         //efetua connecao e seleciona db
